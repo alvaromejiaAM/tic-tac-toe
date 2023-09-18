3f3a008031c84df3d693865c62f40fa1ca8290de
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-09-18 16:25:06 -0500

    Add button grid and array

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-const createPlayer = (name, tile) =>{
-  return {name, tile}
-};
+const createPlayers = (name, tile, turn) =>{
+  return {name, tile, turn}
+  }
 
 const gameBoard = (() =>{
-  const board = () => [];
-  return{board};
-})();
+  let container = document.querySelector('.game');
+  let array = []
+
+  for(let i = 0; i < 9; i++){
+    let box = document.createElement('button');
+    box.classList.add(i);
+    array[i] = 0;
+    container.appendChild(box);
+  }
+
+})()
+
+const displayController =(() =>{
+  const player1 = createPlayers('player1', 'x', true);
+  const player2 = createPlayers('player2', 'o', false);
+  
+})()
