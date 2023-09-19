a472fad4f7cc9dbcb42783c6b9d7b01289500494
alvaro mejia <alvaro.mejiaam01@gmail.com>
2023-09-19 10:41:51 -0500

    Add ability to place tiles depending on turn
    It will determine whose turn it is and will save tile
    positions inside an array and output onto the board.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const createPlayers = (name, tile, turn) =>{
   return {name, tile, turn}
   }
@@ -6,6 +7,7 @@ const gameBoard = (() =>{
   let container = document.querySelector('.game');
   let array = []
 
+  //Create the board and array
   for(let i = 0; i < 9; i++){
     let box = document.createElement('button');
     box.classList.add(i);
@@ -13,10 +15,38 @@ const gameBoard = (() =>{
     container.appendChild(box);
   }
 
-})()
+  return{array};
 
-const displayController =(() =>{
+})();
+
+const displayController = (() =>{
+
   const player1 = createPlayers('player1', 'x', true);
   const player2 = createPlayers('player2', 'o', false);
   
-})()
+  const board = gameBoard;
+  let box = document.querySelectorAll('.game > button');
+  box.forEach(square => {
+    square.addEventListener('click', (e)=>{
+      let index = e.target.classList;
+      parseInt(index);
+      whoTurn = getTurn(player1, player2);
+      board[index] = whoTurn;
+      e.target.innerText = whoTurn;
+    });
+  })
+
+  function getTurn(player1, player2){
+    if(player1.turn === true){
+      player1.turn = false;
+      player2.turn = true;
+      return player1.tile;
+    }
+    else{
+      player1.turn = true;
+      player2.turn = false;
+      return player2.tile;
+    }
+  }
+
+})();
